b793ac195bbb76f4b658e24c869e435c6b759d92
Emmanuel <emmanuel@gmail.com>
2026-03-17 12:20:00 -0600

    Pruebas de Implementacion base de datos prueba

--- PreguntadosUASLP/FormFutbol.Designer.cs
+++ PreguntadosUASLP/FormFutbol.Designer.cs
@@ -28,12 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormFutbol";
+            labelpregFutbol = new Label();
+            botonOp1 = new Button();
+            button2 = new Button();
+            botonOp3 = new Button();
+            botonOp4 = new Button();
+            SuspendLayout();
+            // 
+            // labelpregFutbol
+            // 
+            labelpregFutbol.AutoSize = true;
+            labelpregFutbol.Location = new Point(359, 33);
+            labelpregFutbol.Name = "labelpregFutbol";
+            labelpregFutbol.Size = new Size(38, 15);
+            labelpregFutbol.TabIndex = 0;
+            labelpregFutbol.Text = "label1";
+            labelpregFutbol.Click += labelpregFutbol_Click;
+            // 
+            // botonOp1
+            // 
+            botonOp1.Location = new Point(255, 143);
+            botonOp1.Name = "botonOp1";
+            botonOp1.Size = new Size(75, 23);
+            botonOp1.TabIndex = 1;
+            botonOp1.Text = "button1";
+            botonOp1.UseVisualStyleBackColor = true;
+            botonOp1.Click += botonOp1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(465, 143);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 2;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // botonOp3
+            // 
+            botonOp3.Location = new Point(255, 200);
+            botonOp3.Name = "botonOp3";
+            botonOp3.Size = new Size(75, 23);
+            botonOp3.TabIndex = 3;
+            botonOp3.Text = "button3";
+            botonOp3.UseVisualStyleBackColor = true;
+            botonOp3.Click += botonOp3_Click;
+            // 
+            // botonOp4
+            // 
+            botonOp4.Location = new Point(465, 200);
+            botonOp4.Name = "botonOp4";
+            botonOp4.Size = new Size(75, 23);
+            botonOp4.TabIndex = 4;
+            botonOp4.Text = "button4";
+            botonOp4.UseVisualStyleBackColor = true;
+            botonOp4.Click += botonOp4_Click;
+            // 
+            // FormFutbol
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(botonOp4);
+            Controls.Add(botonOp3);
+            Controls.Add(button2);
+            Controls.Add(botonOp1);
+            Controls.Add(labelpregFutbol);
+            Name = "FormFutbol";
+            Text = "FormFutbol";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label labelpregFutbol;
+        private Button botonOp1;
+        private Button button2;
+        private Button botonOp3;
+        private Button botonOp4;
     }
 }
--- PreguntadosUASLP/FormFutbol.cs
+++ PreguntadosUASLP/FormFutbol.cs
@@ -5,14 +5,139 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace PreguntadosUASLP
 {
     public partial class FormFutbol : Form
     {
+        string connStr = "Server=127.0.0.1;Database=preguntados_uaslp;User ID=root;Password=tu_contraseña;";
+        int idPreguntaActual;
+        int respuestaCorrectaIndex;
         public FormFutbol()
         {
+            
             InitializeComponent();
+            CargarPregunta();
+            
+        }
+
+        void CargarPregunta()
+        {
+            MySqlConnection conn = new MySqlConnection(connStr);
+
+            try
+            {
+                conn.Open();
+
+                string query = "SELECT * FROM preguntas WHERE id_categoria = 6 ORDER BY RAND() LIMIT 1";
+
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    idPreguntaActual = Convert.ToInt32(reader["id_pregunta"]);
+                    labelpregFutbol.Text = reader["pregunta"].ToString();
+                }
+
+                reader.Close();
+                conn.Close();
+
+                CargarRespuestas();
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        void CargarRespuestas()
+        {
+            MySqlConnection conn = new MySqlConnection(connStr);
+
+            try
+            {
+                conn.Open();
+
+                string query = "SELECT * FROM respuestas WHERE id_pregunta = @id";
+
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@id", idPreguntaActual);
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                List<string> opciones = new List<string>();
+                int index = 0;
+
+                while (reader.Read())
+                {
+                    opciones.Add(reader["respuesta"].ToString());
+
+                    if (Convert.ToBoolean(reader["es_correcta"]))
+                    {
+                        respuestaCorrectaIndex = index;
+                    }
+
+                    index++;
+                }
+
+                reader.Close();
+                conn.Close();
+
+                botonOp1.Text = opciones[0];
+                button2.Text = opciones[1];
+                botonOp3.Text = opciones[2];
+                botonOp4.Text = opciones[3];
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            VerificarRespuesta(1);
+
+        }
+
+        private void labelpregFutbol_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void botonOp1_Click(object sender, EventArgs e)
+        {
+            VerificarRespuesta(0);
+
+        }
+
+        private void botonOp3_Click(object sender, EventArgs e)
+        {
+            VerificarRespuesta(2);
+
+        }
+
+        private void botonOp4_Click(object sender, EventArgs e)
+        {
+            VerificarRespuesta(3);
+
+        }
+
+        void VerificarRespuesta(int seleccion)
+        {
+            if (seleccion == respuestaCorrectaIndex)
+            {
+                MessageBox.Show("Correcto");
+            }
+            else
+            {
+                MessageBox.Show("Incorrecto");
+            }
+            CargarPregunta();
         }
     }
 }
