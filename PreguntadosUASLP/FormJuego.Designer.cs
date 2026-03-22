47f6f63c380987b60ce2049ee48d3b886e9944ee
Gali <galilea@gmail.com>
2026-03-21 23:53:30 -0600

    ajustes en el layout de FormJuego

--- PreguntadosUASLP/FormJuego.Designer.cs
+++ PreguntadosUASLP/FormJuego.Designer.cs
@@ -33,6 +33,11 @@
             label1 = new Label();
             label2 = new Label();
             label3 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            label4 = new Label();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
@@ -41,7 +46,7 @@
             label_pregunta.AutoSize = true;
             label_pregunta.BackColor = Color.Lavender;
             label_pregunta.Font = new Font("Arial Rounded MT Bold", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label_pregunta.Location = new Point(426, 111);
+            label_pregunta.Location = new Point(440, 136);
             label_pregunta.Name = "label_pregunta";
             label_pregunta.Size = new Size(284, 32);
             label_pregunta.TabIndex = 0;
@@ -52,9 +57,9 @@
             // 
             pictureBox1.BackColor = Color.Lavender;
             pictureBox1.ErrorImage = Properties.Resources.preguntas1;
-            pictureBox1.Location = new Point(274, 77);
+            pictureBox1.Location = new Point(133, 97);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(609, 108);
+            pictureBox1.Size = new Size(901, 108);
             pictureBox1.TabIndex = 1;
             pictureBox1.TabStop = false;
             // 
@@ -67,7 +72,7 @@
             label1.Name = "label1";
             label1.Size = new Size(191, 23);
             label1.TabIndex = 2;
-            label1.Text = "(vidas sobrantes?)";
+            label1.Text = "(num de pregunta)";
             label1.Click += label1_Click;
             // 
             // label2
@@ -75,11 +80,11 @@
             label2.AutoSize = true;
             label2.BackColor = Color.Lavender;
             label2.Font = new Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label2.Location = new Point(455, 667);
+            label2.Location = new Point(455, 654);
             label2.Name = "label2";
-            label2.Size = new Size(255, 23);
+            label2.Size = new Size(242, 23);
             label2.TabIndex = 3;
-            label2.Text = "(reemplazar con puntaje)";
+            label2.Text = "(btn para sig. pregunta)";
             // 
             // label3
             // 
@@ -88,9 +93,56 @@
             label3.Font = new Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
             label3.Location = new Point(12, 19);
             label3.Name = "label3";
-            label3.Size = new Size(224, 23);
+            label3.Size = new Size(118, 23);
             label3.TabIndex = 4;
-            label3.Text = "(numero de pregunta)";
+            label3.Text = "(categoria)";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(243, 267);
+            button1.Name = "button1";
+            button1.Size = new Size(324, 126);
+            button1.TabIndex = 5;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(596, 267);
+            button2.Name = "button2";
+            button2.Size = new Size(324, 126);
+            button2.TabIndex = 6;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(596, 413);
+            button3.Name = "button3";
+            button3.Size = new Size(324, 126);
+            button3.TabIndex = 7;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(243, 413);
+            button4.Name = "button4";
+            button4.Size = new Size(324, 126);
+            button4.TabIndex = 8;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Lavender;
+            label4.Font = new Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(455, 600);
+            label4.Name = "label4";
+            label4.Size = new Size(255, 23);
+            label4.TabIndex = 9;
+            label4.Text = "(reemplazar con puntaje)";
             // 
             // FormJuego
             // 
@@ -100,6 +152,11 @@
             BackgroundImage = Properties.Resources.fondoMorado;
             BackgroundImageLayout = ImageLayout.Zoom;
             ClientSize = new Size(1179, 775);
+            Controls.Add(label4);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
@@ -121,5 +178,10 @@
         private Label label1;
         private Label label2;
         private Label label3;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Label label4;
     }
 }
